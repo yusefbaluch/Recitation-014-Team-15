b551f35fa30bf4476e52f6eed96b29027d1bd88a
yusefbaluch <root@DESKTOP-4EBBE3G>
2023-09-14 17:29:58 -0600

    Updated readme. Added Template files

---
+++ README.md
@@ -0,0 +1,7 @@
+# lab3-MergeConflict
+Merge Conflict - Tutorial
+
+No  |  FIRSTNAME   |  LASTNAME | GITHUB_PROFILE
+--- |  ---         |   ---     |  ---
+1   |  Yusef       |ALbalushi  | yusefbaluch
+1   |   Tanmay      | Meti |    Tanmay-Meti
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+// returns a vector with true for numbers less than the second parameters and false for those greater than or equal to
+std::vector<bool> LessMask(std::vector<double> nums, double less_than);
+
+// returns a vector with true for numbers greater than the second parameters and false for those less than or equal to
+std::vector<bool> GreaterMask(std::vector<int> nums, int greater_than);
+
+int main(){
+    //call each of the above functions
+    return 0;
+}
