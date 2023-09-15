2b4205f899d2deb3522fec86965ad74c4b82fa94
yusefbaluch <yual1945@colorado.edu>
2023-09-14 18:29:25 -0600

    Version main : 9

--- main.cpp
+++ main.cpp
@@ -9,10 +9,5 @@ std::vector<bool> GreaterMask(std::vector<int> nums, int greater_than);
 
 int main(){
     //call each of the above functions
-<<<<<<< HEAD
-    return 1;
+    return 9;
 }
-=======
-    return 2;
-}
->>>>>>> main
